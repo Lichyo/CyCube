3e7b24801db29cefad30dca494dcc49f9d6b5078
LI,CHI-YU <111233017+Lichyo@users.noreply.github.com>
2023-12-13 14:59:01 +0800

    Update Cube UI & ReadMe.md, init firebase (#2)
    * fix conflic
    
    * update readme.md
    
    * fixec cover overlap problem by calculating offset
    
    * write with simulator
    
    * firebase init complete

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'components/cube_face.dart';
+import 'dart:math';
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:math';
-import 'components/cube_face.dart';
+import 'package:image_picker/image_picker.dart';
+import 'service/cube/cube_rotation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,22 +21,25 @@ class RubiksCube extends StatefulWidget {
 
 class _RubiksCubeState extends State<RubiksCube> {
   Offset _offset = Offset.zero;
+  String? name;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: GestureDetector(
         onPanUpdate: (detail) {
-          setState(() => _offset += detail.delta);
+          setState(() => _offset += detail.delta * 0.4);
         },
         child: Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             elevation: 5,
             title: Text(
               'The Cube',
               style: GoogleFonts.aboreto(
                 fontSize: 27.0,
-                fontWeight: FontWeight.w400
+                fontWeight: FontWeight.w400,
               ),
             ),
           ),
@@ -43,67 +50,32 @@ class _RubiksCubeState extends State<RubiksCube> {
                 origin: const Offset(0, 0),
                 alignment: Alignment.center,
                 transform: Matrix4.identity()
-                  ..rotateX(_offset.dy * pi / 180)
+                  ..rotateX(-_offset.dy * pi / 180)
                   ..rotateY(_offset.dx * pi / 180)
                   ..setEntry(2, 2, 0.001),
                 child: Center(
                   child: Stack(
-                    children: [
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 0.0, -150.0),
-                        child: const CubeFace(
-                          color: Colors.white,
-                        ),
-                      ),
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 0.0, 150.0),
-                        child: const CubeFace(
-                          color: Colors.yellow,
-                        ),
-                      ),
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 0.0, -150.0)
-                          ..rotateY(-pi / 2),
-                        child: const CubeFace(
-                          color: Colors.green,
-                        ),
-                      ),
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(300.0, 0.0, -150.0)
-                          ..rotateY(-pi / 2),
-                        child: const CubeFace(
-                          color: Colors.blue,
-                        ),
-                      ),
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 0.0, -150.0)
-                          ..rotateX(pi / 2),
-                        child: const CubeFace(
-                          color: Colors.red,
-                        ),
-                      ),
-                      Transform(
-                        origin: const Offset(0, 0),
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 300.0, -150.0)
-                          ..rotateX(pi / 2),
-                        child: const CubeFace(
-                          color: Colors.orange,
-                        ),
-                      ),
-                    ],
+                    children: CubeRotation().permutateCube(offset: _offset),
                   ),
                 ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              OutlinedButton(
+                onPressed: () async {
+                  ImagePicker imagePicker = ImagePicker();
+                  XFile? image =
+                      await imagePicker.pickImage(source: ImageSource.gallery);
+                  Uint8List byte = await image!.readAsBytes();
+                  // File('/Users/lichyo/StudioProjects/cube/assets/samples/sample.png')
+                  //     .writeAsBytes(byte);
+                  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+                  fireStore.collection('image').add({
+                    'byte': byte,
+                  });
+                },
+                child: const Text('Open Camera'),
               ),
             ],
           ),
