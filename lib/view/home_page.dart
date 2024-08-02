3910861af1bc34c58ab4a6fe5471dddd1a919a16
Lichyo <lichyo003@gmail.com>
2024-08-02 21:02:41 +0800

    doc : update ReadMe.md

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -11,6 +11,7 @@ import 'dart:math';
 import 'package:gap/gap.dart';
 import 'package:cy_cube/service/database_service.dart';
 import 'package:cy_cube/cube/cube_model/single_cube_component_face_model.dart';
+import 'dart:async';
 
 class RubiksCube extends StatefulWidget {
   const RubiksCube({super.key});
@@ -29,6 +30,17 @@ class _RubiksCubeState extends State<RubiksCube> {
   bool isArrangedLeft = false;
   int arrangeCountX = 0;
   int arrangeCountY = 0;
+  double dy = 0;
+  double dx = 0;
+  bool isSendWarning = false;
+
+  void showSnackBar(BuildContext context, String text) {
+    final snackBar = SnackBar(
+      content: Text(text),
+      duration: const Duration(seconds: 4),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   void initState() {
@@ -49,218 +61,236 @@ class _RubiksCubeState extends State<RubiksCube> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GestureDetector(
-        onPanUpdate: (detail) {
-          setState(() {
-            _offset += detail.delta;
-            // print('cube : ${(_offset.dx / 90).floor()}, counter : $arrangeCountX');
-            if ((_offset.dx / 90).floor() < arrangeCountX - 1) {
-              arrangeCountX--;
-              cubeState.arrangeCube('right');
-            } else if ((_offset.dx / 90).floor() > arrangeCountX - 1) {
-              arrangeCountX++;
-              cubeState.arrangeCube('left');
-            }
-            // print('arrangeCountY : $arrangeCountY, offset : ${(_offset.dy / 90).floor() + 1}');
-            if ((_offset.dy / 90).floor() + 1 > 0 && arrangeCountY == 0) {
-              print('section 1');
-              arrangeCountY++;
-              cubeState.arrangeCube('up');
-            } else if ((_offset.dy / 90).floor() + 1 == 0 && arrangeCountY == 1) {
-              print('section 2');
-              arrangeCountY--;
-              cubeState.arrangeCube('down');
-            }
-          });
-        },
-        child: Scaffold(
-          drawer: Drawer(
-            child: ListView(
-              children: [
-                const DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'CyCube',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-                ListTile(
-                  title: const Text('Reset'),
-                  onTap: () {
-                    cubeState = CubeState();
-                    setState(() {});
-                  },
-                ),
-                ListTile(
-                  title: const Text('Detection ( OpenCV )'),
-                  onTap: () {
-                    setState(() {});
-                  },
-                ),
-                ListTile(
-                  title: const Text('Detection ( Manual )'),
-                  onTap: () async {
-                    var data = await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const CubeSetupPage(),
+      home: Builder(
+        builder: (BuildContext context) {
+          return GestureDetector(
+            onPanUpdate: (detail) {
+              setState(() {
+                _offset += detail.delta;
+                if (arrangeCountY == 0) {
+                  dx = _offset.dx;
+                }
+
+                if (_offset.dy < 30 && _offset.dy > -30) {
+                  dy += detail.delta.dy;
+                }
+
+                if ((dx / 90).floor() < arrangeCountX - 1 &&
+                    arrangeCountY == 0) {
+                  arrangeCountX--;
+                  cubeState.arrangeCube('right');
+                } else if ((dx / 90).floor() > arrangeCountX - 1 &&
+                    arrangeCountY == 0) {
+                  arrangeCountX++;
+                  cubeState.arrangeCube('left');
+                } else if (arrangeCountY != 0 && isSendWarning == false) {
+                  showSnackBar(context, '請將方塊翻正後再做左右翻動');
+                  isSendWarning = true;
+                }
+
+                if ((_offset.dy / 90).floor() + 1 > 0 && arrangeCountY == 0) {
+                  arrangeCountY++;
+                  cubeState.arrangeCube('up');
+                } else if ((_offset.dy / 90).floor() + 1 == 0 &&
+                    arrangeCountY == 1) {
+                  arrangeCountY--;
+                  cubeState.arrangeCube('down');
+                }
+              });
+            },
+            child: Scaffold(
+              drawer: Drawer(
+                child: ListView(
+                  children: [
+                    const DrawerHeader(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
                       ),
-                    );
-                    List<List<SingleCubeComponentFaceModel>> cubeFaces =
-                        data[0];
-                    cubeState.setupCubeWithScanningColor(cubeFaces);
-                    setState(() {});
-                  },
+                      child: Text(
+                        'CyCube',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: const Text('Reset'),
+                      onTap: () {
+                        cubeState = CubeState();
+                        setState(() {});
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('Detection ( OpenCV )'),
+                      onTap: () {
+                        setState(() {});
+                      },
+                    ),
+                    ListTile(
+                      title: const Text('Detection ( Manual )'),
+                      onTap: () async {
+                        var data = await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const CubeSetupPage(),
+                          ),
+                        );
+                        List<List<SingleCubeComponentFaceModel>> cubeFaces =
+                            data[0];
+                        cubeState.setupCubeWithScanningColor(cubeFaces);
+                        setState(() {});
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Join Room'),
-                        content: const Text('Enter room ID to join a room'),
-                        actions: [
-                          TextField(
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                            onChanged: (value) {
-                              roomID = value;
-                            },
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('cancel'),
-                              ),
-                              TextButton(
-                                child: const Text('Join'),
-                                onPressed: () async {
-                                  await DatabaseService.joinRoom(
-                                    email: 'lichyo003@gmail.com',
-                                    roomID: roomID!,
-                                    cubeState: cubeState,
-                                  );
-                                  isJoinCourseRoom = true;
-                                  setState(() {});
-                                  Navigator.pop(context);
+              ),
+              appBar: AppBar(
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Join Room'),
+                            content: const Text('Enter room ID to join a room'),
+                            actions: [
+                              TextField(
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                                onChanged: (value) {
+                                  roomID = value;
                                 },
                               ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('cancel'),
+                                  ),
+                                  TextButton(
+                                    child: const Text('Join'),
+                                    onPressed: () async {
+                                      await DatabaseService.joinRoom(
+                                        email: 'lichyo003@gmail.com',
+                                        roomID: roomID!,
+                                        cubeState: cubeState,
+                                      );
+                                      isJoinCourseRoom = true;
+                                      setState(() {});
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              ),
                             ],
-                          ),
-                        ],
+                          );
+                        },
                       );
                     },
-                  );
-                },
-                icon: const Icon(Icons.door_back_door_outlined),
-              ),
-            ],
-            elevation: 5,
-            title: Text(
-              roomID ?? 'The Cube',
-              style: GoogleFonts.aboreto(
-                fontSize: 27.0,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            centerTitle: true,
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const MaxGap(200),
-              Transform(
-                origin: const Offset(0, 0),
-                alignment: Alignment.center,
-                transform: Matrix4.identity()
-                  ..rotateX(_offset.dy * pi / 180)
-                  ..rotateY(_offset.dx * pi / 180)
-                  ..setEntry(2, 2, 0.001),
-                child: Center(
-                  child: Cube(),
-                ),
-              ),
-              const Gap(100),
-              Visibility(
-                visible: !isJoinCourseRoom,
-                child: CubeRotationTable(
-                  onPressed: (rotation) {
-                    cubeState.rotate(rotation);
-                    if (roomID != null) {
-                      DatabaseService.courseWithStudentPOV(
-                          rotation: rotation, roomID: roomID!);
-                    }
-                  },
-                ),
-              ),
-              Visibility(
-                visible: !isJoinCourseRoom && !isCreateRoom,
-                child: TextButton(
-                  onPressed: () async {
-                    roomID = await DatabaseService.createRoom(
-                      email: 'lichyo003@gmail.com',
-                      cubeState: cubeState,
-                    );
-                    isCreateRoom = true;
-                    setState(() {});
-                  },
-                  child: const Text('create room'),
-                ),
-              ),
-              const MaxGap(100),
-              MaterialButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return CubeStateIn2D(cubeState: cubeState);
-                    },
-                  );
-                  cubeState.show2DFace(facing: Facing.top);
-                },
-                child: Image.asset(
-                  'images/cube_icon.png',
-                  width: 80,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      cubeState.arrangeCube('left');
-                      setState(() {});
-                    },
-                    child: const Text('arrange left'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      cubeState.arrangeCube('right');
-                      setState(() {});
-                    },
-                    child: const Text('arrange right'),
+                    icon: const Icon(Icons.door_back_door_outlined),
                   ),
                 ],
+                elevation: 5,
+                title: Text(
+                  roomID ?? 'The Cube',
+                  style: GoogleFonts.aboreto(
+                    fontSize: 27.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                centerTitle: true,
               ),
-            ],
-          ),
-        ),
+              body: Builder(
+                builder: (BuildContext context) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const MaxGap(200),
+                      Transform(
+                        origin: const Offset(0, 0),
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..rotateX(dy * pi / 180)
+                          ..rotateY(dx * pi / 180)
+                          ..setEntry(2, 2, 0.001),
+                        child: Center(
+                          child: Cube(),
+                        ),
+                      ),
+                      const Gap(100),
+                      Visibility(
+                        visible: !isJoinCourseRoom,
+                        child: CubeRotationTable(
+                          onPressed: (rotation) {
+                            cubeState.rotate(rotation);
+                            if (roomID != null) {
+                              DatabaseService.courseWithStudentPOV(
+                                  rotation: rotation, roomID: roomID!);
+                            }
+                          },
+                        ),
+                      ),
+                      Visibility(
+                        visible: !isJoinCourseRoom && !isCreateRoom,
+                        child: TextButton(
+                          onPressed: () async {
+                            roomID = await DatabaseService.createRoom(
+                              email: 'lichyo003@gmail.com',
+                              cubeState: cubeState,
+                            );
+                            isCreateRoom = true;
+                            setState(() {});
+                          },
+                          child: const Text('create room'),
+                        ),
+                      ),
+                      const MaxGap(100),
+                      MaterialButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return CubeStateIn2D(cubeState: cubeState);
+                            },
+                          );
+                          cubeState.show2DFace(facing: Facing.top);
+                        },
+                        child: Image.asset(
+                          'images/cube_icon.png',
+                          width: 80,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              // showSnackBar(context, 'arrange left');
+                            },
+                            child: const Text('arrange left'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              cubeState.arrangeCube('right');
+                              setState(() {});
+                            },
+                            child: const Text('arrange right'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
