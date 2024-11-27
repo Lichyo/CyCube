5ec82f86199eda053abc0c5a6660a3aa2000acf0
Lichyo <lichyo003@gmail.com>
2024-11-27 15:38:59 +0800

    Temp : fail

--- lib/cube/cube_model/single_cube_model.dart
+++ lib/cube/cube_model/single_cube_model.dart
@@ -13,15 +13,4 @@ class SingleCubeModel {
     required this.z,
   });
 
-  void reset({
-    required CubeComponent component,
-    required double x,
-    required double y,
-    required double z,
-  }) {
-    this.component = component;
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
 }
