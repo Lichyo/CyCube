aa661c5cc4204dcae1646b3c2a6d70f85565dc4f
Lichyo <lichyo003@gmail.com>
2024-12-04 20:41:00 +0800

    Temp

--- lib/view/course_page.dart
+++ lib/view/course_page.dart
@@ -3,16 +3,15 @@ import 'package:cy_cube/cube/cube_state.dart';
 import 'package:cy_cube/service/database_service.dart';
 import 'package:cy_cube/service/image_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'dart:async';
-import 'dart:convert';
 import 'package:cy_cube/config.dart';
 import 'package:cy_cube/cube/cube_view/cube_page.dart';
 import 'package:cy_cube/components/cube_rotation_table.dart';
 import 'package:cy_cube/cube/cube_model/single_cube_component_face_model.dart';
+import 'dart:convert';
 
 class CoursePage extends StatefulWidget {
   const CoursePage({
@@ -28,9 +27,9 @@ class _CoursePageState extends State<CoursePage> {
   late Timer _timer;
   bool isCourseStart = false;
   TextEditingController roomIDController = TextEditingController();
+  String _predictionResult = "";
   bool isLoad = false;
   bool isJoinRoom = false;
-  String _predictionResult = "";
   String role = "teacher";
   String connectionStatus = "Unconnected";
 
@@ -41,8 +40,13 @@ class _CoursePageState extends State<CoursePage> {
     _initializeSocket();
   }
 
-  void startCourse() {
+  void studentStartCourse() {
     _initializeSocket();
+    _socket.on('rotation', (data) {
+      setState(() {
+        _predictionResult = data.toString();
+      });
+    });
     if (ImageController.controller != null) {
       ImageController.controller!.dispose();
     }
@@ -51,12 +55,19 @@ class _CoursePageState extends State<CoursePage> {
         setState(() {});
       }
     });
-    // _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-    //   ImageController.convertCameraImageToJpeg(ImageController.imageBuffer!)
-    //       .then((value) {
-    //     _socket.emit('rotation', base64Encode(value));
-    //   });
-    // });
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      ImageController.convertCameraImageToJpeg(ImageController.imageBuffer!)
+          .then((value) {
+        _socket.emit('save_image', base64Encode(value));
+      });
+    });
+  }
+
+  void teacherStartCourse() {
+    _initializeSocket();
+    setState(() {
+      _socket.emit('receive_image');
+    });
   }
 
   void _initializeSocket() {
@@ -69,12 +80,6 @@ class _CoursePageState extends State<CoursePage> {
       setState(() {
         connectionStatus = "Connected";
       });
-    });
-    _socket.on('rotation', (data) {
-      setState(() {
-        _predictionResult = data.toString();
-      });
-      // }
     });
   }
 
@@ -102,129 +107,135 @@ class _CoursePageState extends State<CoursePage> {
                                   .rotate(rotation: rotation);
                               await DatabaseService
                                   .updateCubeStateWithStudentPOV(
-                                      rotation: rotation,
-                                      roomID: roomIDController.text,);
+                                rotation: rotation,
+                                roomID: roomIDController.text,
+                              );
                             },
                           ),
                         ],
                       )
-                    : Column(
-                        children: [
-                          const MaxGap(200),
-                          CubePage(),
-                          const Gap(100),
-                        ],
-                      ),
+                    : Stack(
+                      children: [
+                        Positioned.fill(
+                            child: Stack(
+                              children: [
+                                if (ImageController.imageInWidget1 != null)
+                                  Opacity(
+                                    opacity: ImageController.toggle ? 1.0 : 0.0,
+                                    child: ImageController.imageInWidget1!,
+                                  ),
+                                if (ImageController.imageInWidget2 != null)
+                                  Opacity(
+                                    opacity: ImageController.toggle ? 0.0 : 1.0,
+                                    child: ImageController.imageInWidget2!,
+                                  ),
+                              ],
+                            ),
+                          ),
+                        CubePage(),
+                      ],
+                    )
               ],
             ),
           )
-        : isLoad
-            ? const Center(child: CircularProgressIndicator())
-            : Column(
-                children: [
-                  const Gap(30),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 16.0),
-                        TextField(
-                          controller: roomIDController,
-                          decoration: InputDecoration(
-                            labelText: 'Room ID',
-                            prefixIcon: const Icon(Icons.numbers),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                          ),
-                          keyboardType: TextInputType.number,
+        : Column(
+            children: [
+              const Gap(30),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      controller: roomIDController,
+                      decoration: InputDecoration(
+                        labelText: 'Room ID',
+                        prefixIcon: const Icon(Icons.numbers),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        const SizedBox(height: 16.0),
-                        const Gap(10),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              setState(() {
-                                role = "teacher";
-                                print(role);
-                              });
-                              await DatabaseService.joinRoom(
-                                roomID: roomIDController.text,
-                                context: context,
-                              );
-                              setState(() {
-                                isJoinRoom = true;
-                                isCourseStart = true;
-                              });
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue, // Background color
-                              foregroundColor: Colors.white, // Text color
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 15.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                            ),
-                            child: const Text('Join the Room ( Teacher )'),
-                          ),
-                        ),
-                        const Gap(10),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              setState(() {
-                                role = "student";
-                                print("student");
-                              });
-                              var data = await Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      CubeSetupPageAuto(socket: _socket),
-                                ),
-                              );
-                              List<List<SingleCubeComponentFaceModel>>
-                                  cubeFaces = data[0];
-                              Provider.of<CubeState>(context, listen: false)
-                                  .setupCubeWithScanningColor(cubeFaces);
-                              roomIDController.text =
-                                  await DatabaseService.createRoom(
-                                      context: context);
-                              print("room ID : " + roomIDController.text);
-                              setState(() {
-                                isJoinRoom = true;
-                                isCourseStart = true;
-                                startCourse();
-                              });
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.grey, // Background color
-                              foregroundColor: Colors.white, // Text color
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 15.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                            ),
-                            child: const Text(
-                                'Cube init & Create the Room ( Student )'),
-                          ),
-                        ),
-                        const Gap(50),
-                        Text(
-                          connectionStatus,
-                          style: const TextStyle(
-                            fontSize: 30,
-                          ),
-                        ),
-                      ],
+                      ),
+                      keyboardType: TextInputType.number,
                     ),
-                  ),
-                ],
-              );
+                    const SizedBox(height: 16.0),
+                    const Gap(10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            role = "teacher";
+                          });
+                          await DatabaseService.joinRoom(
+                            roomID: roomIDController.text,
+                            context: context,
+                          );
+                          setState(() {
+                            isJoinRoom = true;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue, // Background color
+                          foregroundColor: Colors.white, // Text color
+                          padding: const EdgeInsets.symmetric(vertical: 15.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: const Text('Join the Room ( Teacher )'),
+                      ),
+                    ),
+                    const Gap(10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            role = "student";
+                          });
+                          var data = await Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  CubeSetupPageAuto(socket: _socket),
+                            ),
+                          );
+                          List<List<SingleCubeComponentFaceModel>> cubeFaces =
+                              data[0];
+                          Provider.of<CubeState>(context, listen: false)
+                              .setupCubeWithScanningColor(cubeFaces);
+                          roomIDController.text =
+                              await DatabaseService.createRoom(
+                                  context: context);
+                          setState(() {
+                            isJoinRoom = true;
+                            studentStartCourse();
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey, // Background color
+                          foregroundColor: Colors.white, // Text color
+                          padding: const EdgeInsets.symmetric(vertical: 15.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: const Text(
+                            'Cube init & Create the Room ( Student )'),
+                      ),
+                    ),
+                    const Gap(50),
+                    Text(
+                      connectionStatus,
+                      style: const TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
   }
 
   @override
